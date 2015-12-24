9c9a89374cce24fbd13ad9d73bc8548254394445
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2015-12-24 13:27:44 +0100

    Added more queries.

--- queries.sql
+++ queries.sql
@@ -52,6 +52,80 @@ WHERE play.id IN (
 	WHERE p.name = 'Marcelino' OR p.name = 'Guillermo' OR p.name = 'Sito'
 	GROUP BY play.id
 	HAVING COUNT(*) = 3 OR COUNT(*) = 6 --HACK FOR SPACE HULK: DEATH ANGEL
+	-- FIXME: SH:DA is being counted also for solo games
 )
 GROUP BY play.date;
 
+-- WIN% 1vs1
+SELECT p.name, (100.0*SUM(t.winner) / COUNT(*)) AS 'Wins %', COUNT(*) AS 'Plays'
+FROM bgplays_player p
+INNER JOIN bgplays_team_players tp on p.id = tp.player_id
+INNER JOIN bgplays_team t ON tp.team_id = t.id
+WHERE t.play_id IN (
+	SELECT play.id
+	FROM bgplays_play play
+	INNER JOIN bgplays_team t ON play.id = t.play_id
+	INNER JOIN bgplays_team_players tp ON t.id = tp.team_id
+	INNER JOIN bgplays_player p ON p.id = tp.player_id
+	WHERE p.name = 'Marcelino'
+	INTERSECT
+	SELECT play.id
+	FROM bgplays_play play
+	INNER JOIN bgplays_team t ON play.id = t.play_id
+	INNER JOIN bgplays_team_players tp ON t.id = tp.team_id
+	INNER JOIN bgplays_player p ON p.id = tp.player_id
+	WHERE p.name = 'Sito'
+	INTERSECT
+	-- TWO PLAYER COMPETITIVE GAMES
+	SELECT t.play_id
+	FROM bgplays_team t
+	INNER JOIN bgplays_team_players tp ON t.id = tp.team_id
+	WHERE t.winner IS NOT NULL
+	GROUP BY t.play_id
+	HAVING MAX(t.winner) != MIN(t.winner) AND COUNT(tp.player_id) = 2
+)
+GROUP BY p.name;
+
+-- SCORE DIFFERENCE IN EACH PLAY
+SELECT t.play_id, MAX(t.points)-MIN(t.points) 'diff'
+FROM bgplays_play play
+INNER JOIN bgplays_team t ON play.id = t.play_id
+WHERE t.points IS NOT NULL
+GROUP BY t.play_id
+ORDER BY diff;
+
+-- AMOUNT OF COMPETITIVE SECOND PLACES
+SELECT p.name, COUNT(*)
+FROM bgplays_play play
+INNER JOIN bgplays_team t ON play.id = t.play_id
+INNER JOIN bgplays_team_players tp ON t.id = tp.team_id
+INNER JOIN bgplays_player p ON p.id = tp.player_id
+WHERE t.play_id IN (
+        -- NOT ALL WINNERS/LOSERS
+	SELECT play_id FROM bgplays_team
+	WHERE winner IS NOT NULL
+	GROUP BY play_id
+	HAVING MAX(winner) != MIN(winner)
+)
+AND t.play_id IN (
+        -- NOT SAME PLAYER ON DIFFERENT TEAMS
+	SELECT DISTINCT t2.play_id FROM bgplays_team t2
+	INNER JOIN bgplays_team_players tp2 ON tp2.team_id = t2.id
+	GROUP BY t2.play_id, tp2.player_id
+	HAVING COUNT(*) == 1
+)
+AND t.play_id IN (
+        -- AT LEAST THREE TEAMS
+	SELECT play3.id
+	FROM bgplays_play play3
+	INNER JOIN bgplays_team t3 ON play3.id = t3.play_id
+	GROUP BY play3.id
+	HAVING COUNT(t3.id) > 2
+)
+AND t.points IN (
+	-- HIGHEST LOSER SCORE
+	SELECT MAX(t4.points) FROM bgplays_team t4
+	WHERE t4.play_id = t.play_id AND t4.winner = 0
+)
+AND t.winner = 0
+GROUP BY p.name;
