46626e5c53bd89c444653a3d0feb2d645a570079
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2016-02-08 19:11:06 +0100

    Added QuerySet helpers.

--- bgplays/models.py
+++ bgplays/models.py
@@ -1,11 +1,9 @@
 from django.db import models
+from django.db.models import Avg, Count, F, Max, Min, Sum, Q, Prefetch, Case, When
 
 class Game(models.Model):
 	name = models.CharField(max_length=255)
 
-	def cooperatives():
-		print "COOPS!"
-	
 	def __str__(self):
 		return self.name
 
@@ -23,11 +21,20 @@ class Faction(models.Model):
 	def __str__(self):
 		return str((self.game, self.description))
 
+class PlayQuerySet(models.QuerySet):
+	def cooperatives(self):
+		play_ids = Team.objects.filter(winner__isnull=False).values('play_id')\
+			.annotate(Max('winner'), Min('winner'))\
+			.filter(winner__max=F('winner__min')).values('play_id')
+		return self.filter(id__in=play_ids)
+
 class Play(models.Model):
 	game = models.ForeignKey(Game)
 	minutes = models.IntegerField(null=True, blank=True)
 	date = models.DateField(null=True, blank=True)
 	variant = models.ForeignKey(Variant, null=True, blank=True)
+
+	objects = PlayQuerySet.as_manager()
 
 	def __str__(self):
 		return str((self.id, self.game, self.date))
