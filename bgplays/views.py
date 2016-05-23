32cb886282f8516917b4154c299e162888407d4f
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2016-05-23 19:45:42 +0200

    Added skeleton for player view.

--- bgplays/views.py
+++ bgplays/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Player
 
 # Create your views here.
+def player(request, player_name):
+	player = get_object_or_404(Player, name=player_name)
+	context = {'player': player}
+	return render(request, 'bgplays/player.html', context)
+
