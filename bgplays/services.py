7341164d78992a407d20a0705d3a55049cc16bc5
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2016-06-12 21:09:54 +0200

    Added most_played_mates.

--- bgplays/services.py
+++ bgplays/services.py
@@ -1,10 +1,24 @@
 from models import *
 
+# Player info services
 def get_most_played_games(player_id, count=5):
-	plays_ids = Team.objects.filter(players__id=player_id).values('play__id')
-	return Play.objects.filter(id__in=plays_ids)\
-			.annotate(name=F('game__name'))\
-			.values('name')\
-			.annotate(count=Count('name'))\
-			.order_by('-count')[:count]
+    plays_ids = get_play_ids(player_id)
+    return Play.objects.filter(id__in=plays_ids) \
+               .annotate(name=F('game__name')) \
+               .values('name') \
+               .annotate(count=Count('name')) \
+               .order_by('-count')[:count]
 
+def get_most_played_mates(player_id, count=5):
+    plays_ids = get_play_ids(player_id)
+    return Player.objects.filter(team__play__id__in=plays_ids) \
+               .exclude(id=player_id) \
+               .values('name', 'team__play__id') \
+               .distinct() \
+               .values('name') \
+               .annotate(count=Count('name')) \
+               .order_by('-count')[:count]
+
+# Helper methods
+def get_play_ids(player_id):
+    return Play.objects.filter(team__players__id=player_id).values('id').distinct()
--- bgplays/views.py
+++ bgplays/views.py
@@ -4,8 +4,11 @@ import services
 
 # Create your views here.
 def player(request, player_name):
-	player = get_object_or_404(Player, name=player_name)
-	most_played_games = services.get_most_played_games(player.id)
-	context = {'player': player, 'most_played_games': most_played_games}
+	p = get_object_or_404(Player, name=player_name)
+	most_played_games = services.get_most_played_games(p.id)
+	most_played_mates = services.get_most_played_mates(p.id)
+	context = {'player': p,
+               'most_played_games': most_played_games,
+               'most_played_mates': most_played_mates}
 	return render(request, 'bgplays/player.html', context)
 
